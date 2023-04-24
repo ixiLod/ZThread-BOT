ad5fbb2a2817b23a85f2d7694da575a0a71b7a16
Dolix7 <51421090+Dolix7@users.noreply.github.com>
2023-04-24 20:21:41 +0200

    refacto

--- events/addToThread.js
+++ events/addToThread.js
@@ -14,39 +14,39 @@ module.exports = {
     if (!message.channel.guild) return;
     if (message.channel.parent?.type === 'GUILD_CATEGORY') return;
     if (message.id === threadID[0]) return;
-
     // Get the thread ID
-    const threadChannel = client.channels.cache.get(threadID[0]);
-
-    if (!threadChannel) {
-      console.error(`Thread channel with ID "${threadID}" not found.`);
-      message.channel.send(
-        `${message.author}, le message cible n'a pas pu être récupéré, pensez à placer la réaction 🪡 avant de mettre le fil`
-      );
-      return;
-    }
-
-    // Get the thread
-    const thread = await threadChannel.fetch();
-
-    if (!thread) {
-      console.error(`Thread with ID "${threadID}" not found.`);
-      return;
-    }
-
-    // Get the date of the message
-    const date = new Date(message.createdTimestamp);
-    const globalDate = date.toLocaleDateString('fr-FR', {
-      day: '2-digit',
-      month: '2-digit',
-      year: '2-digit',
-    });
-    const hour = date.toLocaleTimeString('fr-FR', {
-      hour: '2-digit',
-      minute: '2-digit',
-    });
-    // Send the message in the thread and delete the original message
     try {
+      const threadChannel = client.channels.cache.get(threadID[0]);
+
+      if (!threadChannel) {
+        console.error(`Thread channel with ID "${threadID}" not found.`);
+        message.channel.send(
+          `${message.author}, le message cible n'a pas pu être récupéré, pensez à placer la réaction 🪡 avant de mettre le fil`
+        );
+        return;
+      }
+
+      // Get the thread
+      const thread = await threadChannel.fetch();
+
+      if (!thread) {
+        console.error(`Thread with ID "${threadID}" not found.`);
+        return;
+      }
+
+      // Get the date of the message
+      const date = new Date(message.createdTimestamp);
+      const globalDate = date.toLocaleDateString('fr-FR', {
+        day: '2-digit',
+        month: '2-digit',
+        year: '2-digit',
+      });
+      const hour = date.toLocaleTimeString('fr-FR', {
+        hour: '2-digit',
+        minute: '2-digit',
+      });
+      // Send the message in the thread and delete the original message
+
       const threadMessage = await thread.send(
         `**${message.author.username}** 💬 *${globalDate} à ${hour}* \n${message.content}\n\n----------\n\n`
       );
